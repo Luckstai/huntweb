2fa42cfcc527e2c2b5dbc0fe4fb4b3102f931e31
Luckstai <lucas.henrique.tai@gmail.com>
2019-11-03 22:02:01 -0300

    added the link component of react-router-dom to handle the routings

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,33 +1,68 @@
 import React, { Component } from "react";
 import api from "../../services/api";
 import "./styles.css";
+
+import { Link } from "react-router-dom";
+
 export default class Main extends Component {
   state = {
-    products: []
+    products: [],
+    productInfo: {},
+    currentPage: 1
   };
   componentDidMount() {
     this.loadProducts();
   }
 
-  loadProducts = async () => {
-    const { data: response } = await api.get(`/products`);
+  loadProducts = async (page = 1) => {
+    const { data } = await api.get(`/products?page=${page}`);
+    const { docs, ...productInfo } = data;
 
-    this.setState({ products: response.docs });
-    console.log(response);
+    this.setState({ products: docs, productInfo, currentPage: page });
+  };
+
+  prevPage = () => {
+    const { currentPage } = this.state;
+
+    if (currentPage === 1) return;
+
+    const pageNumber = currentPage - 1;
+
+    this.loadProducts(pageNumber);
+  };
+
+  nextPage = () => {
+    const { currentPage, productInfo } = this.state;
+
+    if (currentPage === productInfo) return;
+
+    const pageNumber = currentPage + 1;
+
+    this.loadProducts(pageNumber);
   };
 
   render() {
-    const { products } = this.state;
+    const { products, currentPage, productInfo } = this.state;
     return (
       <div className="product-list">
         {products.map(product => (
           <article key={product._id}>
             <strong>{product.title}</strong>
             <p>{product.description}</p>
-
-            <a href="">Acessar</a>
+            <Link to={`/products/${product._id}`}>Acessar </Link>
           </article>
         ))}
+        <div className="actions">
+          <button disabled={currentPage === 1} onClick={this.prevPage}>
+            Anterior
+          </button>
+          <button
+            disabled={currentPage === productInfo.pages}
+            onClick={this.nextPage}
+          >
+            Próxima
+          </button>
+        </div>
       </div>
     );
   }
